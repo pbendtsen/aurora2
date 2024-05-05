238e6d6192068145ce14847d73b5159ecbd87292
bendtsen-visma <peter.bendtsen@visma.com>
2024-05-05 16:15:05 +0200

    build fixed

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ async function fetchData(setNextOpening: any, setOpeningTime: any, setSoedt: any
 
 export default function Home() {
 
-  const keyValuePairs = {
+  const keyValuePairs: any = {
     plain: 13,
     plainVHO: 35,
     plainGK: 35,
@@ -43,7 +43,7 @@ export default function Home() {
     soedtAdditional: 25
   };
 
-  const [newOrder, setNewOrder] = useState({
+  const [newOrder, setNewOrder] = useState<any>({
     name: '', 
     phone: '',
     pickup: '', 
